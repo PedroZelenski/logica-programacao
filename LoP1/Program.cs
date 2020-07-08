7137cc062fbb00a4d6ba163ff89425ded00bc042
Zelenski <zelenski.contato@gmail.com>
2020-07-08 16:35:20 -0300

    first commit

---
+++ LoP1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace LoP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores relacionais e logicos
+            //bool souRico = false;
+            //double salario = 2000.00;
+            //if(souRico || salario >= 50000.00)
+            //{
+            //    Console.WriteLine("Voce e rico!");
+            //}
+
+            //string sobremesa = "Pudim";
+            //bool estouAcimaDoPeso = false;
+            //if (estouAcimaDoPeso || sobremesa == "Pudim");
+            //{
+            //    Console.WriteLine("Comer sobremesa");
+            //}
+
+            //bool termineiTrabalho = false;
+            //bool estouComFome = true;
+            //if (termineiTrabalho && estouComFome)
+            //{
+            //    Console.WriteLine("Almocar!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Continuar trabalhando!");
+            //}
+
+            //int numero = 10;
+            //bool boolean = true;
+            //if (numero != 10 && boolean == true)
+            //{
+            //    Console.WriteLine("A condicao e verdadeira");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("A condicao e falsa");
+            //}
+
+            //aprendendo sobre concatenacao
+            //string nome = "Pedro!";
+            //string idade = "18 anos";
+            //string bemvindo = $"Seja bem vindo, {nome} Sua idade e de: {idade}";
+            //Console.WriteLine(bemvindo);
+
+            //expressoes logicas e constantes
+            //const int constinteiro = 20;
+            //int inteiro1 = 10;
+            //int inteiro2 = 20;
+            //int soma = inteiro1 + inteiro2;
+            //Console.WriteLine(soma);
+            //inteiro1 = 50 + soma;
+            //Console.WriteLine(inteiro1 * 2);
+
+            //aprendendo a declarar variaveis
+            //string hello = "Seja bem vindo a nossa aula!";
+            //int inteiro = 1;
+            //double numero = 1.5;
+            //bool booleano = false;
+
+            //aprendendo sobre array
+            //int[] numeros = new int[5];
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 50;
+            //numeros[3] = 100;
+            //numeros[4] = 200;
+
+        
+       }
+    }
+}
